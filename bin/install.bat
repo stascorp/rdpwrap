742fd143eac4ef9ea4d2e146013445d58ab087a2
binarymaster <x86corez@gmail.com>
2014-12-11 00:45:50 +0300

    Delete old binaries
    New binaries will be in releases

--- bin/install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-RDPWInst -i
-echo ______________________________________________________________
-echo.
-echo You can check RDP functionality with RDPCheck program.
-echo Also you can configure advanced settings with RDPConf program.
-echo.
-pause
--- bin/uninstall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-RDPWInst -u
-echo.
-pause
