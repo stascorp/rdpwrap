425fc02fec3b822e1ca13511f374ea0a91dead6a
binarymaster <x86corez@gmail.com>
2015-07-17 02:39:09 +0300

    New batch scripts

---
+++ res/install.bat
@@ -0,0 +1,14 @@
+@echo off
+if not exist "%~dp0RDPWInst.exe" goto :error
+"%~dp0RDPWInst" -i
+echo ______________________________________________________________
+echo.
+echo You can check RDP functionality with RDPCheck program.
+echo Also you can configure advanced settings with RDPConf program.
+echo.
+goto :anykey
+:error
+echo [-] Installer executable not found.
+echo Please extract all files from the downloaded package or check your anti-virus.
+:anykey
+pause
