f8f6012273b6f7c2c436c5cb3eaf13965ef70475
asmtron <49522124+asmtron@users.noreply.github.com>
2019-08-12 00:42:53 +0000

    Update autoupdate.bat

--- bin/autoupdate.bat
+++ bin/autoupdate.bat
@@ -27,7 +27,7 @@ REM { Special thak to binarymaster, saurav-biswas and all other contributors }
 REM -----------------------------------------
 REM Location of new/updated rdpwrap.ini files
 REM -----------------------------------------
-set rdpwrap_ini_update_github_1="https://raw.githubusercontent.com/asmtron/rdpwrap/patch-1/res/rdpwrap.ini"
+set rdpwrap_ini_update_github_1="https://raw.githubusercontent.com/asmtron/rdpwrap/master/res/rdpwrap.ini"
 set rdpwrap_ini_update_github_2="https://raw.githubusercontent.com/saurav-biswas/rdpwrap-1/patch-1/res/rdpwrap.ini"
 REM set rdpwrap_ini_update_github_3="https://raw.githubusercontent.com/....Extra.3...."
 REM set rdpwrap_ini_update_github_4="https://raw.githubusercontent.com/....Extra.4...."
